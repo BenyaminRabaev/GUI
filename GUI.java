cfe3f459b4760a846f99360f68e712b8da5e3931
Benyamin Rabaev <66232105+BenyaminRabaev@users.noreply.github.com>
2021-04-21 02:24:00 -0400

    Add files via upload

---
+++ GUI.java
@@ -0,0 +1,42 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.BorderFactory;
+import java.awt.GridLayout;
+import java.awt.BorderLayout; 
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import java.awt.event.ActionListener;
+public class GUI implements ActionListener{
+	int count = 0;
+	JLabel label:
+	public GUI(){
+		//defining the parameters
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		panel.setBorder(BorderFactory.createEmptyBorder(30,30,10,30));
+		panel.setLayout(new GridLayout(0,1));
+		frame.add(panel, BorderLayout.CENTER);
+		//button go brrrrrr
+		JButton button = new JButton("click me noob");
+		label = new JLabel("FREAKING CLICK HIM");
+		panel.add(button);
+		panel.add(label);
+		button.addActionListener(this);
+
+		//opening and title
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("Benramins Minecraft Modding IDE");
+		frame.pack();
+		frame.setVisible(true);
+	}
+	//if you dont know what this is your wierd
+	public static void main(String[] args){
+		new GUI();
+
+	//event listener go brrrrrr
+		public void actionPerformed(ActionEvent e);{
+			count += 1;
+			lavel.setText("number of clicks:" + count);
+		}
+	}
+}
